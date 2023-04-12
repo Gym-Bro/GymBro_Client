8e5dee6c895f5ff4e16c60f58d048c6b6dbdbc81
villajul <villajul3@hotmail.comgit config --global user.name villajulgit config --global user.email villajul3@hotmail.com>
2023-04-12 02:08:48 -0300

    template email html in assets

--- app/createUser/page.tsx
+++ app/createUser/page.tsx
@@ -4,7 +4,7 @@ import signUp from "../firebase/auth/signup";
 import Logo from "../../public/favicon.ico";
 import Image from "next/image";
 import { Modal } from "../components/Modal";
-import css from './createUser.module.css'
+import css from "./createUser.module.css";
 
 function Page() {
   const [tokenID, setTokenID] = React.useState("");
@@ -16,7 +16,7 @@ function Page() {
     password: "",
     repassword: "",
   });
-console.log(file)
+  console.log(file);
   const createUser = async () => {
     fetch("http://localhost:3000/auth/register", {
       method: "POST",
@@ -37,7 +37,9 @@ console.log(file)
     const idToken = await result?.user.getIdToken();
     if (idToken === "") setTokenID(idToken);
     createUser();
-    error ? alert('su usuario no pudo ser creado') : alert('su usuario se creo correctamente')
+    error
+      ? alert("su usuario no pudo ser creado")
+      : alert("su usuario se creo correctamente");
   };
 
   const handleInputChange = ({
@@ -45,105 +47,108 @@ console.log(file)
   }: React.ChangeEvent<HTMLInputElement>) => {
     setInput({ ...input, [name]: value });
   };
-  const handleFileChange = ({target:{files}}:React.ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = ({
+    target: { files },
+  }: React.ChangeEvent<HTMLInputElement>) => {
     const input = files && files[0];
-    console.log(input)
+    console.log(input);
     if (!input) return;
     const data = new FormData();
-    data.append('foto', input.name)
+    data.append("foto", input.name);
     setFile(data);
-    console.log('formdata',data)
+    console.log("formdata", data);
   };
 
   return (
-    
-      <Modal>
-    <div className="container">
+    <Modal>
+      <div className="container">
         <div className={css.primaryContainer}>
-        <Image alt="Gym-Bro" src={Logo} className={css.img} />
-      <div className={css.container}>
-        <h3>Registro</h3>        
-        <form onSubmit={handleForm} className={css.form}>
-          <label htmlFor="name">
-            <p>Nombre:</p>
-            <input
-              onChange={handleInputChange}
-              required
-              minLength={3}
-              value={input.name}
-              type="name"
-              name="name"
-              id="name"
-              placeholder="  Nombre..."
-            />
-          </label>
-          <label htmlFor="lastname">
-            <p>Apellido:</p>
-            <input
-              onChange={handleInputChange}
-              required
-              minLength={3}
-              value={input.lastname}
-              type="name"
-              name="lastname"
-              id="lastname"
-              placeholder="  Apellido..."
-            />
-          </label>
-          <label htmlFor="email">
-            <p>Correo electronico:</p>
-            <input
-              onChange={handleInputChange}
-              required
-              value={input.email}
-              type="email"
-              name="email"
-              id="email"
-              placeholder="  Correo..."
-            />
-          </label>
-          <label htmlFor="password">
-            <p>Contraseña:</p>
-            <input
-              onChange={handleInputChange}
-              required
-              minLength={8}
-              maxLength={24}
-              value={input.password}
-              type="password"
-              name="password"
-              id="password"
-              placeholder="  Contraseña..."
-            />
-          </label>
-          <label htmlFor="repassword">
-            <p>Confirmar Contraseña:</p>
-            <input
-              onChange={handleInputChange}
-              required
-              
-              value={input.repassword}
-              type="password"
-              name="repassword"
-              id="repassword"
-              placeholder="  Confirmar contraseña..."
-            />            
-          </label>
-          <label htmlFor="user-image">
-            <p>Foto de perfil:</p>
-            <input type="file" 
-            onChange={handleFileChange}
-            accept="image/png, image/jpeg"
-            id="file" 
-            name="file"
-            />
-          </label>
-          <p>Estas listo para empezar?</p>
-          <button type="submit" className="btn">Registrarse</button>
-        </form>
+          <Image alt="Gym-Bro" src={Logo} className={css.img} />
+          <div className={css.container}>
+            <h3>Registro</h3>
+            <form onSubmit={handleForm} className={css.form}>
+              <label htmlFor="name">
+                <p>Nombre:</p>
+                <input
+                  onChange={handleInputChange}
+                  required
+                  minLength={3}
+                  value={input.name}
+                  type="name"
+                  name="name"
+                  id="name"
+                  placeholder="  Nombre..."
+                />
+              </label>
+              <label htmlFor="lastname">
+                <p>Apellido:</p>
+                <input
+                  onChange={handleInputChange}
+                  required
+                  minLength={3}
+                  value={input.lastname}
+                  type="name"
+                  name="lastname"
+                  id="lastname"
+                  placeholder="  Apellido..."
+                />
+              </label>
+              <label htmlFor="email">
+                <p>Correo electronico:</p>
+                <input
+                  onChange={handleInputChange}
+                  required
+                  value={input.email}
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="  Correo..."
+                />
+              </label>
+              <label htmlFor="password">
+                <p>Contraseña:</p>
+                <input
+                  onChange={handleInputChange}
+                  required
+                  minLength={8}
+                  maxLength={24}
+                  value={input.password}
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="  Contraseña..."
+                />
+              </label>
+              <label htmlFor="repassword">
+                <p>Confirmar Contraseña:</p>
+                <input
+                  onChange={handleInputChange}
+                  required
+                  value={input.repassword}
+                  type="password"
+                  name="repassword"
+                  id="repassword"
+                  placeholder="  Confirmar contraseña..."
+                />
+              </label>
+              <label htmlFor="user-image">
+                <p>Foto de perfil:</p>
+                <input
+                  type="file"
+                  onChange={handleFileChange}
+                  accept="image/png, image/jpeg"
+                  id="file"
+                  name="file"
+                />
+              </label>
+              <p>Estas listo para empezar?</p>
+              <button type="submit" className="btn">
+                Registrarse
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
     </Modal>
   );
 }
