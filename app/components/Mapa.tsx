46b08b50f37f7d08807626ac3f848b672eb20eb5
villajul <villajul3@hotmail.comgit config --global user.name villajulgit config --global user.email villajul3@hotmail.com>
2023-04-24 02:58:23 -0300

    sedes listas

--- app/components/Mapa.tsx
+++ app/components/Mapa.tsx
@@ -1,35 +1,61 @@
 import React from "react";
-import { GoogleMap, LoadScript, Marker,  } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 interface MapProps {
-  address: string;
+  position?: google.maps.LatLngLiteral;
 }
 
-const Map: React.FC<MapProps> = ({ address }) => {
-  const [position, setPosition] = React.useState<google.maps.LatLngLiteral | undefined>(undefined);
-
-  React.useEffect(() => {
-    const geocoder = new google.maps.Geocoder()
-     geocoder.geocode({ address }, (results, status) => {
-      if (status === "OK" && results && results[0]) {
-        setPosition(results[0].geometry.location.toJSON());
-      } else {
-        console.error(`Geocode error: ${status}`);
-      }
-    });
-  }, [address]);
+const Map: React.FC<MapProps> = ({ position }) => {
+  const defaultCenter = { lat: 37.7749, lng: -122.4194 }; // San Francisco
+  const mapPosition = position ?? defaultCenter; // Use the given position, or default to San Francisco
 
   return (
     <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}>
-      {position ? (
-        <GoogleMap mapContainerStyle={{ height: "400px", width: "400px" }} zoom={15} center={position}>
-          <Marker position={position} />
-        </GoogleMap>
-      ) : (
-        <div>Loading...</div>
-      )}
+      <GoogleMap
+        mapContainerStyle={{ height: "400px", width: "100%" }}
+        zoom={15}
+        center={mapPosition}
+      >
+        <Marker position={mapPosition} />
+      </GoogleMap>
     </LoadScript>
   );
 };
 
 export default Map;
+
+// import React from "react";
+// import { GoogleMap, LoadScript, Marker,  } from "@react-google-maps/api";
+
+// interface MapProps {
+//   address: string;
+// }
+
+// const Map: React.FC<MapProps> = ({ address }) => {
+//   const [position, setPosition] = React.useState<google.maps.LatLngLiteral | undefined>(undefined); 
+//   React.useEffect(() => {
+//     const geocoder = new google.maps.Geocoder()
+//     console.log(google.maps)
+//      geocoder.geocode({ address }, (results, status) => {
+//       if (status === "OK" && results && results[0]) {
+//         setPosition(results[0].geometry.location.toJSON());
+//       } else {
+//         console.error(`Geocode error: ${status}`);
+//       }
+//     });
+//   }, [address]);
+
+//   return (
+//     <LoadScript googleMapsApiKey={'AIzaSyBkEyCcixFvp0P3swFcWBBy8qjC09yRneY'}>
+//       {position ? (
+//         <GoogleMap mapContainerStyle={{ height: "400px", width: "400px" }} zoom={15} center={position}>
+//           <Marker position={position} />
+//         </GoogleMap>
+//       ) : (
+//         <div>Loading...</div>
+//       )}
+//     </LoadScript>
+//   );
+// };
+
+// export default Map;
--- app/sedes/page.tsx
+++ app/sedes/page.tsx
@@ -4,20 +4,20 @@ import Map from "../components/Mapa";
 import css from "./sedes.module.css"
 
 function Page() {
-  const [first, setfirst] = React.useState("Florida, C1005, Microcentro, CABA");
+  const [first, setfirst] = React.useState({ lat: -34.5989486, lng: -58.3781122 });
   const [second, setSecond] = React.useState(
-    "Monteagudo 295, C1437EVE, Parque Patricios, CABA"
-  );
+    { lat: -34.5967481, lng: -58.386465}
+  );//-,-
   return (
     <div className={css.container}>
       <h2>Podés encontrarnos en las siguientes sedes:</h2>
       <div className={css.map}>
         <h3>Florida, C1005, Microcentro, CABA</h3>
-        <Map address={first} />
+        <Map position={first} />
       </div>
       <div className={css.map}>
         <h3>Monteagudo 295, C1437EVE, Parque Patricios, CABA</h3>
-        <Map address={second} />
+        <Map position={second} />
       </div>
     </div>
   );
